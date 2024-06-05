5baa176291979a3907fb1f585c26d523cbb1d729
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-05 16:54:49 -0700

    create: CommentForm component and scss

---
+++ src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,29 @@
+import "./CommentForm.scss";
+import Avatar from "../Avatar/Avatar";
+import Button from "../Button/Button";
+
+const CommentForm = () => {
+  return (
+    <form className="comment-form">
+      <div className="comment-form__avatar">
+        <Avatar isDefault={false} />
+      </div>
+      <div className="comment-form__content">
+        <div className="comment-form__field">
+          <label htmlFor="comment" className="comment-form__label label-text">
+            JOIN THE CONVERSATION
+          </label>
+          <input
+            className="comment-form__input"
+            placeholder="Add a new comment"
+          ></input>
+        </div>
+        <div className="comment-form__button">
+          <Button buttonText="COMMENT" icon="comment" />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default CommentForm;
