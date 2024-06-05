727e0cb14e42630ee28ea350adaff5926c46bee6
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-05 11:43:17 -0700

    add: props to Video Card component and passed props

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,16 +1,13 @@
 import "./VideoCard.scss";
-import dummyImage from "../../assets/images/Upload-video-preview.jpg";
-const VideoCard = () => {
+const VideoCard = ({ video }) => {
   return (
     <div className="video-card">
       <div className="video-card__image-container">
-        <img className="video-card__image" src={dummyImage} />
+        <img className="video-card__image" src={video.image} />
       </div>
       <div className="video-card__body">
-        <h2 className="video-card__title">
-          The Future of Artificial Intelligence
-        </h2>
-        <p className="video-card__channel">Aiden Thompson</p>
+        <h2 className="video-card__title">{video.title}</h2>
+        <p className="video-card__channel">{video.channel}</p>
       </div>
     </div>
   );
