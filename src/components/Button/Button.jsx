3f4a1e84c5f09fe046552716a376ff4f343db329
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-04 22:04:19 -0700

    update: button component for both comments and upload

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,13 @@
 import upload from "../../assets/icons/upload.svg";
+import comment from "../../assets/icons/add_comment.svg";
 import "./Button.scss";
 
-const Button = () => {
+const Button = ({ icon, buttonText }) => {
+  const buttonIcon = icon == "upload" ? upload : comment;
   return (
     <button className="button label-text">
-      <img className="button__icon" src={upload} />
-      <p className="button__body">UPLOAD</p>
+      <img className="button__icon" src={buttonIcon} />
+      <p className="button__body">{buttonText}</p>
     </button>
   );
 };
