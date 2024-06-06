edee62200fe7430481386e158a463d4713cfe398
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-06 12:21:15 -0700

    create: CommentSection Component

---
+++ src/components/CommentSection/CommentSection.jsx
@@ -0,0 +1,16 @@
+import Comment from "../Comment/Comment";
+import CommentForm from "../CommentForm/CommentForm";
+import "./CommentSection.scss";
+
+const CommentSection = ({ comments }) => {
+  return (
+    <section className="comment-section section-wrapper">
+      <p className="comment-section__title">{comments.length} Comments</p>
+      <CommentForm />
+      {comments.map((comment) => {
+        return <Comment key={comment.id} comment={comment} />;
+      })}
+    </section>
+  );
+};
+export default CommentSection;
