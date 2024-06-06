aa383b3401f8219f195d31a50fbb14d53c20ece4
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-06 12:22:22 -0700

    delete: old comment section and replace with new CommentSection component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,16 @@ import videoData from "./data/video-details.json";
 import "./App.css";
 
 import Navbar from "../src/components/Navbar/Navbar.jsx";
-import Comment from "./components/Comment/Comment";
 import VideoCard from "./components/VideoCard/VideoCard.jsx";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer.jsx";
-import CommentForm from "./components/CommentForm/CommentForm.jsx";
 import VideoDetails from "./components/VideoDetails/VideoDetails.jsx";
+import CommentSection from "./components/CommentSection/CommentSection.jsx";
 
 function App() {
   const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
   const [unselectedVideos, setUnselectedVideos] = useState(videoData.slice(1));
 
-  function getSelectedVideo(id) {
+  function handleSelectedVideo(id) {
     const selectedVideo = videoData.find((video) => video.id === id);
     const otherVideos = videoData.filter((video) => video.id !== id);
 
@@ -31,15 +30,7 @@ function App() {
         <section className="video-content">
           <div className="video-content__body">
             <VideoDetails video={selectedVideo} />
-            <section className="comment-section section-wrapper">
-              <p className="comment__title">
-                {selectedVideo.comments.length} Comments
-              </p>
-              <CommentForm />
-              {selectedVideo.comments.map((comment) => {
-                return <Comment key={comment.id} comment={comment} />;
-              })}
-            </section>
+            <CommentSection comments={selectedVideo.comments} />
           </div>
           <aside className="recommend section-wrapper">
             <p className="recommend__title label-text">NEXT VIDEOS</p>
@@ -50,7 +41,7 @@ function App() {
                     key={video.id}
                     video={video}
                     id={video.id}
-                    handleClick={() => getSelectedVideo(video.id)}
+                    handleClick={() => handleSelectedVideo(video.id)}
                   />
                 );
               })}
