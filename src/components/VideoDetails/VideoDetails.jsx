acaee8c7f2e5d462b3fee41e80f349b7e4153ea8
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-05 17:16:18 -0700

    create: VideoDetails component and scss

---
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -0,0 +1,44 @@
+import "./VideoDetails.scss";
+import viewIcon from "../../assets/icons/views.svg";
+import likeIcon from "../../assets/icons/likes.svg";
+import { convertTimestamp } from "../../utils/utils";
+
+const VideoDetails = ({ video }) => {
+  const { title, channel, timestamp, views, likes, description } = video;
+  return (
+    <div className="video-details section-wrapper">
+      <h1 className="video-details__title">{title}</h1>
+      <div className="video-details__metadata">
+        <div className="video-details__metadata--left">
+          <div className="video-details__author">
+            <p className="video-details__info">By {channel}</p>
+          </div>
+          <div className="video-details__date">
+            <p className="video-details__info">{convertTimestamp(timestamp)}</p>
+          </div>
+        </div>
+        <div className="video-details__metadata--right">
+          <div className="video-details__views">
+            <img
+              className="video-details__icon"
+              src={viewIcon}
+              alt="View icon"
+            />
+            <p className="video-details__info">{views}</p>
+          </div>
+          <div className="video-details__likes">
+            <img
+              className="video-details__icon"
+              src={likeIcon}
+              alt="like icon"
+            />
+            <p className="video-details__info">{likes}</p>
+          </div>
+        </div>
+      </div>
+      <p className="video-details__copy">{description}</p>
+    </div>
+  );
+};
+
+export default VideoDetails;
