95426128b9ddb1cad7b9055a872353baacd2e02e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-09 16:51:51 -0700

    create: home page and move elements from App.js to a Home Component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import videoData from "./data/video-details.json";
+import "./Home.scss";
+
+import Navbar from "../src/components/Navbar/Navbar.jsx";
+import VideoCard from "./components/VideoCard/VideoCard.jsx";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer.jsx";
+import VideoDetails from "./components/VideoDetails/VideoDetails.jsx";
+import CommentSection from "./components/CommentSection/CommentSection.jsx";
+
+const Home = () => {
+  const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
+  const [unselectedVideos, setUnselectedVideos] = useState(videoData.slice(1));
+
+  function handleSelectedVideo(id) {
+    const selectedVideo = videoData.find((video) => video.id === id);
+    const otherVideos = videoData.filter((video) => video.id !== id);
+
+    setSelectedVideo(selectedVideo);
+    setUnselectedVideos(otherVideos);
+  }
+  return (
+    <>
+      <Navbar />
+      <main className="main-content">
+        <VideoPlayer image={selectedVideo.image} />
+        <section className="video-content">
+          <div className="video-content__body">
+            <VideoDetails video={selectedVideo} />
+            <CommentSection comments={selectedVideo.comments} />
+          </div>
+          <aside className="recommend section-wrapper">
+            <p className="recommend__title label-text">NEXT VIDEOS</p>
+            <div className="recommend__list">
+              {unselectedVideos.map((video) => {
+                return (
+                  <VideoCard
+                    key={video.id}
+                    video={video}
+                    id={video.id}
+                    handleClick={() => handleSelectedVideo(video.id)}
+                  />
+                );
+              })}
+            </div>
+          </aside>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export default Home;
