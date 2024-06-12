5cb74a4a588ae61312ba1fa1a8b64f4d0656aad0
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-11 19:13:51 -0700

    chore: restructure Video Upload Page and update scss for the new structure

--- src/pages/VideoUpload/VideoUpload.jsx
+++ src/pages/VideoUpload/VideoUpload.jsx
@@ -6,48 +6,47 @@ const VideoUpload = () => {
   return (
     <main className="upload section-wrapper">
       <h1 className="upload__title">Upload Video</h1>
-      <div className="upload__body">
-        <section className="upload__thumbnail-section">
-          <p className="upload__label label-text">VIDEO THUMBNAIL</p>
-          <figure className="upload__preview">
-            <img
-              className="upload__image"
-              src={VideoThumbnail}
-              alt="video image preview"
-            />
-          </figure>
-        </section>
-        <section className="upload__form-section">
-          <form className="upload__form">
-            <fieldset className="upload__form-inner">
-              <label htmlFor="title" className="upload__label label-text">
-                TITLE YOUR VIDEO
-              </label>
-              <input
-                type="text"
-                id="title"
-                name="title"
-                placeholder="Add a title to your video"
-                className="upload__input"
+      <form className="upload__form">
+        <div className="upload__body">
+          <section className="upload__thumbnail-section">
+            <p className="upload__label label-text">VIDEO THUMBNAIL</p>
+            <figure className="upload__preview">
+              <img
+                className="upload__image"
+                src={VideoThumbnail}
+                alt="video image preview"
               />
+            </figure>
+          </section>
 
-              <label htmlFor="description" className="label-text upload__label">
-                ADD A VIDEO DESCRIPTION
-              </label>
-              <textarea
-                id="description"
-                name="description"
-                placeholder="Add a description of your video"
-                className="upload__input--textarea"
-              />
-            </fieldset>
-            <div className="upload__buttons">
-              <Button icon="publish" text="PUBLISH" isPrimary={true} />
-              <Button icon="none" text="CANCEL" isPrimary={false} />
-            </div>
-          </form>
-        </section>
-      </div>
+          <section className="upload__form-section">
+            <label htmlFor="title" className="upload__label label-text">
+              TITLE YOUR VIDEO
+            </label>
+            <input
+              type="text"
+              id="title"
+              name="title"
+              placeholder="Add a title to your video"
+              className="upload__input"
+            />
+
+            <label htmlFor="description" className="label-text upload__label">
+              ADD A VIDEO DESCRIPTION
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              placeholder="Add a description of your video"
+              className="upload__input--textarea"
+            />
+          </section>
+        </div>
+        <div className="upload__footer">
+          <Button icon="publish" text="PUBLISH" isPrimary={true} />
+          <Button icon="none" text="CANCEL" isPrimary={false} />
+        </div>
+      </form>
     </main>
   );
 };
