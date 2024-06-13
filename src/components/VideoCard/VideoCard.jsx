e97a1eee736c6077b630fe4f5a7dad68814d4d65
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-12 17:40:41 -0700

    add: skeleton color to match style guide

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,31 +1,33 @@
 import "./VideoCard.scss";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const VideoCard = ({ video = {}, handleClick, isLoading }) => {
   const { image, title, channel } = video;
   return (
-    <article className="video-card" onClick={handleClick}>
-      <div
-        className={
-          isLoading ? "video-card--loading" : "video-card__image-container"
-        }
-      >
-        {isLoading ? (
-          <Skeleton />
-        ) : (
-          <img
-            className="video-card__image"
-            src={image}
-            alt={`${title} Video Thumbnail`}
-          />
-        )}
-      </div>
-      <div className="video-card__body">
-        <h3 className="video-card__title">{title || <Skeleton />}</h3>
-        <p className="video-card__channel">{channel || <Skeleton />}</p>
-      </div>
-    </article>
+    <SkeletonTheme color="#E1E1E1" highlightColor="#E1E1E1">
+      <article className="video-card" onClick={handleClick}>
+        <div
+          className={
+            isLoading ? "video-card--loading" : "video-card__image-container"
+          }
+        >
+          {isLoading ? (
+            <Skeleton />
+          ) : (
+            <img
+              className="video-card__image"
+              src={image}
+              alt={`${title} Video Thumbnail`}
+            />
+          )}
+        </div>
+        <div className="video-card__body">
+          <h3 className="video-card__title">{title || <Skeleton />}</h3>
+          <p className="video-card__channel">{channel || <Skeleton />}</p>
+        </div>
+      </article>
+    </SkeletonTheme>
   );
 };
 
--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -2,52 +2,54 @@ import "./VideoDetails.scss";
 import viewIcon from "../../assets/icons/views.svg";
 import likeIcon from "../../assets/icons/likes.svg";
 import { convertTimestamp } from "../../utils/utils";
-import Skeleton from "react-loading-skeleton";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 const VideoDetails = ({ video, isLoading }) => {
   const { title, channel, timestamp, views, likes, description } = video;
   return (
-    <article className="video-details section-wrapper">
-      <h1 className="video-details__title">{title || <Skeleton />}</h1>
-      <section className="video-details__metadata">
-        <div className="video-details__metadata--left">
-          <div className="video-details__author">
-            <p className="video-details__info">
-              {channel ? `By ${channel}` : <Skeleton />}
-            </p>
-          </div>
-          <div className="video-details__date">
-            <p className="video-details__info">
-              {timestamp ? convertTimestamp(timestamp) : <Skeleton />}
-            </p>
-          </div>
-        </div>
-        {!isLoading && (
-          <div className="video-details__metadata--right">
-            <div className="video-details__views">
-              <img
-                className="video-details__icon"
-                src={viewIcon}
-                alt="View icon"
-              />
-              <p className="video-details__info">{views}</p>
+    <SkeletonTheme color="#E1E1E1" highlightColor="#E1E1E1">
+      <article className="video-details section-wrapper">
+        <h1 className="video-details__title">{title || <Skeleton />}</h1>
+        <section className="video-details__metadata">
+          <div className="video-details__metadata--left">
+            <div className="video-details__author">
+              <p className="video-details__info">
+                {channel ? `By ${channel}` : <Skeleton />}
+              </p>
             </div>
-            <div className="video-details__likes">
-              <img
-                className="video-details__icon"
-                src={likeIcon}
-                alt="like icon"
-              />
-              <p className="video-details__info">{likes}</p>
+            <div className="video-details__date">
+              <p className="video-details__info">
+                {timestamp ? convertTimestamp(timestamp) : <Skeleton />}
+              </p>
             </div>
           </div>
-        )}
-      </section>
-      <p className="video-details__copy">
-        {description || <Skeleton count={5} />}
-      </p>
-    </article>
+          {!isLoading && (
+            <div className="video-details__metadata--right">
+              <div className="video-details__views">
+                <img
+                  className="video-details__icon"
+                  src={viewIcon}
+                  alt="View icon"
+                />
+                <p className="video-details__info">{views}</p>
+              </div>
+              <div className="video-details__likes">
+                <img
+                  className="video-details__icon"
+                  src={likeIcon}
+                  alt="like icon"
+                />
+                <p className="video-details__info">{likes}</p>
+              </div>
+            </div>
+          )}
+        </section>
+        <p className="video-details__copy">
+          {description || <Skeleton count={5} />}
+        </p>
+      </article>
+    </SkeletonTheme>
   );
 };
 
