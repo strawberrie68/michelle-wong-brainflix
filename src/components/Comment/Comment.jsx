7c2afb417f2935d20d1da7b902a097a92332f97a
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-12 21:05:36 -0700

    add: state for comment form and handleCommentSubmit

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,18 +1,30 @@
 import Avatar from "../Avatar/Avatar";
 import { convertTimestamp } from "../../utils/utils";
 import "./Comment.scss";
+import deleteIcon from "../../assets/icons/delete.svg";
 
-const Comment = ({ commentData }) => {
-  const { name, timestamp, comment } = commentData;
+const Comment = ({ commentData, handleDeleteComment }) => {
+  const { name, timestamp, comment, id } = commentData;
+
   return (
-    <article className="comment">
+    <article className="comment" id={id}>
       <div className="comment__avatar">
         <Avatar isDefault={true} />
       </div>
       <div className="comment__details">
         <div className="comment__info">
           <p className="comment__author">{name}</p>
-          <p className="comment__date">{convertTimestamp(timestamp)}</p>
+          <div className="comment__metadata">
+            <p className="comment__date">{convertTimestamp(timestamp)}</p>
+            <img
+              className="comment__delete"
+              src={deleteIcon}
+              alt="delete icon button"
+              onClick={(event) =>
+                handleDeleteComment(event.target.closest(".comment").id)
+              }
+            />
+          </div>
         </div>
         <div className="comment__copy">{comment}</div>
       </div>
