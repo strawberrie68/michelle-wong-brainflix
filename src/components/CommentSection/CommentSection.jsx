c4be8b674fd036464fc0d339039d8bfe40eb8b29
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-12 17:24:13 -0700

    add: skeleton loader to project and add conditional

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,14 +1,18 @@
+import Skeleton from "react-loading-skeleton";
 import Comment from "../Comment/Comment";
 import CommentForm from "../CommentForm/CommentForm";
 import "./CommentSection.scss";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const CommentSection = ({ comments }) => {
   if (comments === undefined) {
-    return <p>Loading comments...</p>;
+    return <p className="section-wrapper">Loading comments...</p>;
   }
   return (
     <section className="comment-section section-wrapper">
-      <p className="comment-section__title">{comments.length} Comments</p>
+      <p className="comment-section__title">
+        {comments ? `${comments.length} Comments` : <Skeleton />}{" "}
+      </p>
       <CommentForm />
       {comments.map((comment) => {
         return <Comment key={comment.id} commentData={comment} />;
--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,18 +1,29 @@
 import "./VideoCard.scss";
-const VideoCard = ({ video, handleClick }) => {
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const VideoCard = ({ video = {}, handleClick, isLoading }) => {
   const { image, title, channel } = video;
   return (
     <article className="video-card" onClick={handleClick}>
-      <div className="video-card__image-container">
-        <img
-          className="video-card__image"
-          src={image}
-          alt={`${title} Video Thumbnail`}
-        />
+      <div
+        className={
+          isLoading ? "video-card--loading" : "video-card__image-container"
+        }
+      >
+        {isLoading ? (
+          <Skeleton />
+        ) : (
+          <img
+            className="video-card__image"
+            src={image}
+            alt={`${title} Video Thumbnail`}
+          />
+        )}
       </div>
       <div className="video-card__body">
-        <h3 className="video-card__title">{title}</h3>
-        <p className="video-card__channel">{channel}</p>
+        <h3 className="video-card__title">{title || <Skeleton />}</h3>
+        <p className="video-card__channel">{channel || <Skeleton />}</p>
       </div>
     </article>
   );
