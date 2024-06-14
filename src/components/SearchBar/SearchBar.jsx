a3d49a3b6a756b0ee071f278995a0ff8da5e07a0
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-14 14:42:31 -0700

    fix: search bar component focus css

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,28 @@
 import searchIcon from "../../assets/icons/search.svg";
 import "./SearchBar.scss";
+import { useRef } from "react";
 
 const SearchBar = () => {
+  const searchRef = useRef();
+
+  const handleFocus = () => {
+    searchRef.current.classList.add("focused");
+  };
+
+  const handleBlur = () => {
+    searchRef.current.classList.remove("focused");
+  };
+
   return (
-    <div className="search">
+    <div tabIndex="0" className="search" ref={searchRef}>
       <img className="search__icon" src={searchIcon} alt="search icon" />
-      <input className="search__input" type="text" placeholder="Search"></input>
+      <input
+        className="search__input"
+        type="text"
+        placeholder="Search"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+      ></input>
     </div>
   );
 };
