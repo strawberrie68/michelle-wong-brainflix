180862c7e79e1575b96a31a1193fe693276c8f63
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-14 14:43:41 -0700

    fix: css margin for alert component

--- src/components/Alert/Alert.jsx
+++ src/components/Alert/Alert.jsx
@@ -1,7 +1,11 @@
 import "./Alert.scss";
 
 const Alert = ({ type, message }) => {
-  return <div className={`alert alert--${type}`}>{message}</div>;
+  return (
+    <div className={`alert alert--${type}`}>
+      <p className="alert__body">{message}</p>
+    </div>
+  );
 };
 
 export default Alert;
