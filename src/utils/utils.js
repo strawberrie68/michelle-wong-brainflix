a7364fda9e0841525ec0ca485fdc51f95b7ee37e
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2024-06-17 21:08:22 -0700

    add: helper function to format the like number

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,3 +2,7 @@ export function convertTimestamp(timestamp) {
   const date = new Date(timestamp);
   return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
 }
+
+export function formatLikes(likes) {
+  return (Number(likes.replace(/,/g, "")) + 1).toLocaleString();
+}
